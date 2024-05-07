06a8f00d44d3e51fe97b2d52b4d0ae59cf779659
rchavez <rchavez@bytesw.com>
2024-05-06 19:51:35 -0600

    se agrega api de dialogflow, funcionalidades para llamado de script y se crea carpeta para indicar id del boto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { ChatbotComponent } from './chatbot/chatbot.component';
+import { FormsModule } from '@angular/forms';
+const routes: Routes = [
+  { path: '', component: ChatbotComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), FormsModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
